e74af9f9d08200fc0e88ca9695d1be6510d8a22e
juelsonjunior <juelsonjunior27@gmail.com>
2025-06-19 22:55:13 +0100

    feat:add others routes reports

--- src/routes/dailyReport.js
+++ src/routes/dailyReport.js
@@ -12,4 +12,20 @@ router.post("/reports", (req, res) => {
     blockers,
   });
 });
+
+router.get("/reports", (req, res) => {
+  const { from, to } = req.query;
+
+  res.status(200).json(`Relatorio de ${from} a ${to}`);
+});
+
+router.get("reports/summary/weekly", (req, res) => {
+  res.status(200).json("Relatorio semanal gerado com sucesso");
+});
+
+router.get("reports/summary/weekly", (req, res) => {
+  const { format } = req.query;
+
+  res.status(200).json(`Relatorio gerado no formato ${format}`);
+});
 export default router;
