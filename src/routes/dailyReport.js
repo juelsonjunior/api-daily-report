4c1d5140ee6d8011b5ca344fe8b188e004a37347
juelsonjunior <juelsonjunior27@gmail.com>
2025-06-19 23:09:27 +0100

    fix:resolve problem of path route

--- src/routes/dailyReport.js
+++ src/routes/dailyReport.js
@@ -19,13 +19,14 @@ router.get("/reports", (req, res) => {
   res.status(200).json(`Relatorio de ${from} a ${to}`);
 });
 
-router.get("reports/summary/weekly", (req, res) => {
+router.get("/reports/summary/weekly", (req, res) => {
   res.status(200).json("Relatorio semanal gerado com sucesso");
 });
 
-router.get("reports/summary/weekly", (req, res) => {
+router.get("/reports/summary/weekly", (req, res) => {
   const { format } = req.query;
 
   res.status(200).json(`Relatorio gerado no formato ${format}`);
 });
+
 export default router;
