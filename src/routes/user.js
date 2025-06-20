2e4b2db8a02be10e65b1688a827b2d23b72b3f86
juelsonjunior <juelsonjunior27@gmail.com>
2025-06-20 03:53:29 +0100

    refcator:add bcrypt in password user registed

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import bcrypt from "bcrypt";
+
 import User from "../models/User.js";
 
 const router = Router();
@@ -16,22 +18,36 @@ router.post("/login", (req, res) => {
 router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
 
-  if (!name || !email || !password) {
-    return res.status(400).json(`Precisa preenche todos os campos`);
+  try {
+    if (!name || !email || !password) {
+      return res.status(400).json(`Precisa preenche todos os campos`);
+    }
+
+    const userDuplicated = await User.findOne({ name });
+
+    if (userDuplicated) {
+      return res.status(409).json("Esse email já esta em uso");
+    }
+
+    const hasPassword = await bcrypt.hash(password, 8);
+    console.log(`Senha hash ${hasPassword}`);
+
+    const newUser = await User.create({
+      name: name,
+      email: email,
+      password: hasPassword,
+    });
+
+    if (!newUser) {
+      return res
+        .status(400)
+        .json(`Falha ao cadastrar usuario no banco de dados`);
+    }
+    res.status(201).json(`Usuario cadastrado com sucesso`);
+  } catch (error) {
+    res.status(500).json(`Erro: Falha no servidor`, error);
+    console.log("O erro", error);
   }
-
-  const userDuplicated = await User.findOne({ name });
-
-  if (userDuplicated) {
-    return res.status(409).json("Esse email já esta em uso");
-  }
-
-  const newUser = await User.create({ name, email, password });
-
-  if (!newUser) {
-    return res.status(400).json(`Falha ao cadastrar usuario no banco de dados`);
-  }
-  res.status(201).json(`Usuario cadastrado com sucesso`);
 });
 
 export default router;
