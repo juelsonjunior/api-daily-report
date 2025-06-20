0d097d19249c2bc566573cbbb0704cfff1a9b2c5
juelsonjunior <juelsonjunior27@gmail.com>
2025-06-20 04:44:09 +0100

    refactor:improve format json response

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,7 +5,8 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 const router = Router();
-const JWTSECRET = "e02bec5bcdba3ca747227627852375ca068d2e3722043471bef14d4afe4e1ca9";
+const JWTSECRET =
+  "e02bec5bcdba3ca747227627852375ca068d2e3722043471bef14d4afe4e1ca9";
 
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
@@ -13,12 +14,14 @@ router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json(`Usuário não encontrado`);
+      return res.status(400).json({ message: "Usuário não encontrado" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json(`Senha incorrecta. Tente novamente!`);
+      return res
+        .status(400)
+        .json({ message: "Senha incorrecta. Tente novamente!" });
     }
 
     const token = jwt.sign({ id: user._id }, JWTSECRET, { expiresIn: "1m" });
@@ -34,13 +37,15 @@ router.post("/register", async (req, res) => {
 
   try {
     if (!name || !email || !password) {
-      return res.status(400).json(`Precisa preenche todos os campos`);
+      return res
+        .status(400)
+        .json({ message: "Precisa preencher todos os campos" });
     }
 
     const userDuplicated = await User.findOne({ name });
 
     if (userDuplicated) {
-      return res.status(409).json("Esse email já esta em uso");
+      return res.status(409).json({ message: "Este email já esta em uso" });
     }
 
     const hasPassword = await bcrypt.hash(password, 8);
@@ -54,11 +59,11 @@ router.post("/register", async (req, res) => {
     if (!newUser) {
       return res
         .status(400)
-        .json(`Falha ao cadastrar usuario no banco de dados`);
+        .json({ message: "Falha ao cadastrar usuario no banco de dados" });
     }
-    res.status(201).json(`Usuario cadastrado com sucesso`);
+    res.status(201).json({ message: "Usuario cadastrado com sucesso" });
   } catch (error) {
-    res.status(500).json(`Falha no servidor`, error);
+    res.status(500).json({ message: "Falha no servidor", error });
   }
 });
 
