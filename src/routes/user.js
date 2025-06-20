6dd38873b65cdc30c05a3efe947df67b01d26c4a
juelsonjunior <juelsonjunior27@gmail.com>
2025-06-20 03:34:33 +0100

    feat:implement create user enpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import User from "../models/User.js";
 
 const router = Router();
 
@@ -12,14 +13,25 @@ router.post("/login", (req, res) => {
     );
 });
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
 
-  res
-    .status(200)
-    .json(
-      `Cadastro feito com o nome ${name} o email ${email} e a senha ${password}`
-    );
+  if (!name || !email || !password) {
+    return res.status(400).json(`Precisa preenche todos os campos`);
+  }
+
+  const userDuplicated = await User.findOne({ name });
+
+  if (userDuplicated) {
+    return res.status(409).json("Esse email já esta em uso");
+  }
+
+  const newUser = await User.create({ name, email, password });
+
+  if (!newUser) {
+    return res.status(400).json(`Falha ao cadastrar usuario no banco de dados`);
+  }
+  res.status(201).json(`Usuario cadastrado com sucesso`);
 });
 
 export default router;
