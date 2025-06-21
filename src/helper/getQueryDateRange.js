11ed3a6cf5c2edffba52c1039876ca4a33e5d856
juelsonjunior <juelsonjunior27@gmail.com>
2025-06-21 03:53:00 +0100

    delete:remove helper

--- src/helper/getQueryDateRange.js
+++
@@ -1,7 +0,0 @@
-export default function getQueryDateRange(from, to) {
-  const startdata = new Date(from);
-  const endData = new Date(to);
-  endData.setDate(endData.getDate() + 1);
-
-  return { startdata, endData };
-}
