4a8efcd9f2d391f0b9df67cf008241a573c7a647
juelsonjunior <juelsonjunior27@gmail.com>
2025-06-21 16:15:44 +0100

    feat:initialize cron for report summary weekly

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import Routes from "./routes/index.js";
 import conectBD from "./database/bd.js";
+import "./cron/weeklyReport.js";
 
 const app = express();
 const PORT = 3000;
@@ -15,5 +16,5 @@ conectBD
     );
   })
   .catch((err) => {
-    console.log("Falha ao conectar com o banco de dados",err);
+    console.log("Falha ao conectar com o banco de dados", err);
   });
