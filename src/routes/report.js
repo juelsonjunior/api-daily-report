6737306dd2a93c5156df548413e5342539481622
juelsonjunior <juelsonjunior27@gmail.com>
2025-06-21 04:21:48 +0100

    refactor:add trycatch in routes and verify idAuthor report

--- src/routes/report.js
+++ src/routes/report.js
@@ -7,65 +7,77 @@ const router = Router();
 router.post("/reports", async (req, res) => {
   const { whatWasDone, whatToDoTomorrow, blockers } = req.body;
 
-  if (!whatWasDone || !whatToDoTomorrow) {
-    return res
-      .status(400)
-      .json({ message: "Precisa preencher todos os campos" });
+  try {
+    if (!whatWasDone || !whatToDoTomorrow) {
+      return res
+        .status(400)
+        .json({ message: "Precisa preencher todos os campos" });
+    }
+
+    const reportDuplicated = await Report.findOne({
+      whatWasDone,
+      whatToDoTomorrow,
+      blockers,
+    });
+
+    if (reportDuplicated) {
+      return res
+        .status(409)
+        .json({ message: "Esse relatório já foi cadastrado" });
+    }
+
+    const newReport = await Report.create({
+      idAuthor: req.user.id,
+      whatWasDone,
+      whatToDoTomorrow,
+      blockers,
+    });
+
+    if (!newReport) {
+      return res
+        .status(400)
+        .json({ message: "Falha ao cadastrar relatório no banco de dados" });
+    }
+
+    res.status(201).json({ message: "Relatório cadastrado com sucesso" });
+  } catch (error) {
+    res.status(500).json({ message: "Falha no servidor" });
+    console.log(error);
   }
-
-  const reportDuplicated = await Report.findOne({
-    whatWasDone,
-    whatToDoTomorrow,
-    blockers,
-  });
-
-  if (reportDuplicated) {
-    return res
-      .status(409)
-      .json({ message: "Esse relatório já foi cadastrado" });
-  }
-
-  const newReport = await Report.create({
-    whatWasDone,
-    whatToDoTomorrow,
-    blockers,
-  });
-
-  if (!newReport) {
-    return res
-      .status(400)
-      .json({ message: "Falha ao cadastrar relatório no banco de dados" });
-  }
-
-  res.status(201).json({ message: "Relatório cadastrado com sucesso" });
 });
 
 router.get("/reports", async (req, res) => {
   const { from, to } = req.query;
 
-  if (!from || !to) {
-    return res
-      .status(400)
-      .json({ message: "Precisa informar o intervalo de busca" });
+  try {
+    if (!from || !to) {
+      return res
+        .status(400)
+        .json({ message: "Precisa informar o intervalo de busca" });
+    }
+
+    if (from > to) {
+      return res
+        .status(400)
+        .json({ message: "A data inicial não pode ser maior que a final" });
+    }
+
+    const { startdata, endData } = getQueryDateRange(from, to);
+
+    const filterData = await Report.find({
+      idAuthor: req.user.id,
+      createAt: { $gte: startdata, $lt: endData },
+    }).select("-idAuthor -_id -__v");
+
+    if (!filterData) {
+      return res.json("Nenhum relatório foi feito nesse intervalo");
+    }
+
+    res.status(200).json(filterData);
+  } catch (error) {
+    res.status(500).json({ message: "Falha no servidor" });
+    console.log(error);
   }
-
-  if (from > to) {
-    return res
-      .status(400)
-      .json({ message: "A data inicial não pode ser maior que a final" });
-  }
-
-  const { startdata, endData } = getQueryDateRange(from, to);
-
-  const filterData = await Report.find({
-    createAt: { $gte: startdata, $lt: endData },
-  });
-
-  if (!filterData) {
-    return res.json("Nenhum relatório foi feito nesse intervalo");
-  }
-
-  res.status(200).json(filterData);
 });
 
 router.get("/reports/summary/weekly", async (req, res) => {
@@ -81,8 +93,9 @@ router.get("/reports/summary/weekly", async (req, res) => {
   const sundayDataClean = sunday.toISOString().split("T")[0];
 
   const reportWeek = await Report.find({
+    idAuthor: req.user.id,
     createAt: { $gte: monday, $lt: sunday },
-  }).select("-_id -__v");
+  }).select("-idAuthor -_id -__v");
 
   const reportSummaryWeekly = {
     user: req.user.name,
